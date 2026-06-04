dc7ecef9bfaed41de55179d9c5be0a68aea9eff9
Aleksandrov.P.P <Guitarherolove@mail.ru>
2026-06-04 08:25:04 +0400

    Удалил лишние файлы и переменные, которые 100% не нужны.

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -1,23 +1,14 @@
-variable "flow" {
-  type    = string
-  default = "24-01"
-}
-
 variable "cloud_id" {
   type    = string
   default = "b1ge810cu67d6o92bcg0" # ID from cloud-guitarherolove in Yandex Cloud
 }
+
 variable "folder_id" {
   type    = string
   default = "b1go149ii9n6ob9ksga3" # ID from default in Yandex Cloud
 }
 
-variable "test" {
-  type = map(number)
-  default = {
-    cores         = 2
-    memory        = 1
-    core_fraction = 20
-  }
+variable "flow" {
+  type    = string
+  default = "24-01"
 }
-
